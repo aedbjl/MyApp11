851e78394fb1a3ad9640ac568914bf5af0a8c9b2
iii-user <iii-user@iii-userde-macbook-4.local>
2017-06-29 10:51:27 +0800

    f

--- MyApp11/ViewController.swift
+++ MyApp11/ViewController.swift
@@ -10,6 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //背景session處理
+    
+    @IBAction func test1(_ sender: Any) {
+        let url = URL(string:"https://tw.yahoo.com")
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = session.dataTask(with: url!, completionHandler: {(data,response,error)in
+            if error == nil{
+                let mydata = String(data: data!, encoding: String.Encoding.utf8)
+                print(mydata)
+            }else{
+                print(error)
+            }
+        
+        
+        
+        })
+        
+        task.resume()
+        
+        
+    }
+    
+    @IBAction func test2(_ sender: Any) {
+        let url = URL(string: "http://127.0.0.1/44444post.php")
+        var req = URLRequest(url: url!)
+    
+        req.httpBody = "account=xxx&passwd=xxx".data(using: String.Encoding.utf8)
+        req.httpMethod = "POST" //GET則body不用寫
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: req, completionHandler: {(data,response,error)in
+            if error == nil{
+                let str = String(data: data!, encoding: .utf8)
+                print(str)
+            }else{
+                print(error)
+            }
+        })
+        task.resume()
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
